61cf129c2bd4d21ec2bab9599af4a307a63a54d6
Chris <christopherhpena02@gmail.com>
2023-09-22 23:50:57 -0400

    trying to figure out why tempTxt is not being considered a string type :/

--- removecss.js
+++ removecss.js
@@ -1,48 +1,49 @@
-window.onload = function() {
-    document.getElementById("btn").onclick = function(){removestyles()};
-    function removestyles(){
-        var txt = document.getElementById("userInputBox").value;
-        var tempTxt = txt;
+window.onload  =  function () {
+    document.getElementById ( "btn" ). onclick  =  function (){ removestyles ()};
+    function   removestyles (){
+        var   txt  =  document.getElementById ( "userInputBox" ).value ;
+        var   tempTxt  =  txt.toString ();
         //iterate through text and stop if we see "<style or style=" "
-        var i=0;
-        var j=0;
-        var y = 0;
+        var   i = 0 ;
+        var   j = 0 ;
+        var   y  =  0 ;
         //setting up temp string to hold chunk of text we will be removing from the larger text
-        var tempString = "";
-        var styleStart = "";
-        var check = true;
-        for(i=0;i<tempTxt.length-3;i++){
+        var   tempString  =  "" ;
+        var   styleStart  =  "" ;
+        var   check  =  true ;
+        for ( i = 0 ; i < tempTxt . length - 3 ; i ++){
             //if we find an < in the html
-            if(tempTxt[i]=="<" ){
-                j = i;
+            if ( tempTxt [ i ]== "<"  ){
+                j  =  i ;
                 //begin a new loop that will stop once we hit the end of the tag
-                while(tempTxt[j] !== ">"){
+                while ( tempTxt [ j ] !==  ">" ){
                     //if we see the beginning of the word style, we know its a style attribute
-                    if(tempTxt[j] == "s" && tempTxt[j+1] == "t" && tempTxt[j+2] == "y"){
+                    if ( tempTxt [ j ] ==  "s"  &&  tempTxt [ j + 1 ] ==  "t"  &&  tempTxt [ j + 2 ] ==  "y" ){
                         //new iterator for going from beginning to end of style attribute
-                        console.log(i, tempt[Txt[j]]);
-                        y =j;
+                        console.log (i , tempTxt [ j ]);
+                        styleStart  =  j
+                        y  = j ;
                         //while we know the if statements are false
-                        while(check == true){
+                        while ( check  ==  true ){
+                            console.log ( tempTxt [ y ],  tempTxt [ y + 1 ]);
                             //if our current character is a double quote followed by a space, we know the style attribute has ended
-                            if(tempTxt[y] == '"' && tempTxt[y+1] ==" "){
-                                tempTxt[y+1] = '';
-                                check = false;
-                            }
+                            if ( tempTxt [ y ] ==  '"'  &&  tempTxt [ y + 1 ] == " " ){
+                                tempTxt  =  tempTxt.subString ( 0 , j - 1 ) +  tempTxt.substring ( y + 1 );
+                                check  =  false ;
+                           }
                             //we also know it has ended if
-                            else if(tempTxt[y]=='"'&& tempTxt[y+1]==">"){
-                                check = false;
-                            }
-                            //replace with empty character :)
-                            tempTxt[y] = '';
-                            y++;
-                        }
-                    }
-                    j++;
-                }
-            }
-        }
-        document.getElementById("result").innerText = tempTxt;
-
-    }
+                            else   if ( tempTxt [ y ]== '"' &&  tempTxt [ y + 1 ]== ">" ){
+                                tempTxt  =  tempTxt.subString ( 0 , j - 1 ) +  tempTxt.substring ( y + 1 );
+                                check  =  false ;
+                           }
+                            console . log ( y );
+                            y ++;
+                       }
+                   }
+                    j ++;
+               }
+           }
+       }
+        document . getElementById ( "result" ). innerText  =  tempTxt ;
+   }
 }
