5df0e44f6e214b2e8c73999b94610b0a21b09d23
Chris <christopherhpena02@gmail.com>
2023-09-22 22:05:22 -0400

    finished searching for blocks of style?

--- removecss.js
+++ removecss.js
@@ -6,19 +6,27 @@ window.onload = function() {
         //iterate through text and stop if we see "<style or style=" "
         var i=0;
         var j=0;
+        var y = 0;
         //setting up temp string to hold chunk of text we will be removing from the larger text
         var tempString = "";
+        var styleStart = "";
         for(i=0;i<txt.length-5;i++){
             //if we find an < in the html
             if(txt[i]=="<" ){
                 j = i;
                 //begin a new loop that will stop once we hit the end of the tag
                 while(txt[j] !== ">"){
+                    //if we see the beginning of the word style, we know its a style tag
                     if(txt[j] == "s" && txt[j+1] == "t" && txt[j+2] == "y"){
-                        
+                        //store beginning of style attribute
+                        styleStart = j;
+                        //continue until we hit a double quotation mark followed by a space
+                        for(y=j;txt[y] !== '"' && txt[y+1] !== " ";y++ ){
+                            styleEnd = y;
+                        }
                     }
+                    j++;
                 }
-                //now we know we have found a style=
             }
         }
         document.getElementById("result").innerText = i;
